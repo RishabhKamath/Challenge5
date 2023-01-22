b05834567e31836934d3ed0106775ff3377cd99c
rishabh.kamath <rshbhk22@gmail.com>
2023-01-22 10:28:29 -0600

    added today's date to the home page

---
+++ Assets/js/script.js
@@ -0,0 +1,28 @@
+// var todayDate = moment().format('dddd, MMM Do YYYY');
+// $("#today").text(todayDate);
+
+// function whatTime() {
+//     var timeNow = moment().hour();
+
+//     $(".time-block").each(function () {
+//         var timeHour = parseInt($(this).attr("id").split("hour")[1]);
+
+//         if (timeHour < timeNow) {
+//             $(this).addClass("pasthour");
+//         }
+//         else if (timeHour === timeNow) {
+//             $(this).addClass("presenthour");
+//         }
+//         else if (timeHour > timeNow) {
+//             $(this).addClass("futurehour");
+//         }
+//     })
+// }
+
+// $(document).ready(function () {
+//     $(".saveBtn").on("click", function () {
+//     })
+
+//     whatTime();
+// })
+
